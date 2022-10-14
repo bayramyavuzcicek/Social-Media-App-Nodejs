3e83eec53ef3f1437ec2c8753b266adf438dc8af
bayramyavuzcicek <cicekbayramyavuz@gmail.com>
2022-10-14 15:39:15 +0300

    DB connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
 const app = express();
 
 
+//dotenv ready to use
+dotenv.config();
 
+//Database connection
+mongoose.connect(process.env.MONGO_URL,()=>{
+    console.log("Connected to database");
+});
+
+
+
+
+
+//server is running
 const PORT = 5000;
-
 app.listen(PORT,()=>{
     console.log(`Backend is running on port ${PORT}`);
 });
