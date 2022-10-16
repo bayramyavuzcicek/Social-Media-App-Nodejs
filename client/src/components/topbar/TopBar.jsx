54482a1f121c66f468fff42deeaec8064da7c579
bayramyavuzcicek <cicekbayramyavuz@gmail.com>
2022-10-16 07:24:57 +0300

    TopBar component

---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,38 @@
+import './topBar.css'
+import {Search,Person,Chat,Notifications} from '@mui/icons-material';
+export default function Topbar() {
+  return (
+    <div className="topBar">
+     <div className="topBarLeft">
+      <span className="logo">YavuzSocial</span>
+     </div>
+     <div className="topBarCenter">
+      <div className="searchBar">
+        <Search className='searchIcon'/>
+        <input placeholder='search for friend, post or any video' className="searchInput" />
+      </div>
+     </div>
+     <div className="topBarRight">
+      <div className="topBarLinks">
+        <span className="topBarLink">Homepage</span>
+        <span className="topBarLink">Timeline</span>
+      </div>
+      <div className="topBarIcons">
+        <div className="topBarIconItem">
+          <Person/>
+          <span className="topBarIconBadge">1</span>
+        </div>
+        <div className="topBarIconItem">
+          <Chat/>
+          <span className="topBarIconBadge">2</span>
+        </div>
+        <div className="topBarIconItem">
+          <Notifications/>
+          <span className="topBarIconBadge">1</span>
+        </div>
+      </div>
+      <img src="/assets/person/1.jpeg" alt="" className='topBarImg'/>
+     </div>
+    </div>
+  )
+}
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import TopBar from '../../components/topbar/TopBar'
+
+export default function Home() {
+  return (
+    
+     <TopBar/>
+    
+  )
+}
